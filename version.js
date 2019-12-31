eff7168abac8905ce4586e8e2fa96a0a2ef136ee
qq542vev <qq542vev@yahoo.co.jp>
2020-01-01 04:25:34 +0900

    bug fix, add version.js

---
+++ version.js
@@ -0,0 +1,7 @@
+callback({
+	"name": "Bookmarklet Generator",
+	"version": "0.1.2",
+	"update": "2020-01-01",
+	"since": "2019-04-12",
+	"downloadURL": "https://purl.org/meta/bookmarklet-generator/download"
+});
